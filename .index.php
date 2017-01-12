8b3491241404746676029470c492a96a1711a662
Ben Smith <rios7467@gmail.com>
2017-01-11 18:51:00 -0700

    Added the html settings and the persona of Jimmy Smtih his use case and part of the interaction flow

---
+++ .index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+		<head>
+
+			<meta charset="UTF-8"  />
+			<title>Reddit Comment on a Post Persona</title>
+		</head>
+	<body>
+		<h1>Persona of Jimmy Smtih</h1>
+		<ul>
+			<li>Name : Jimmy Smtih</li>
+			<li>Age: 16</li>
+			<li>Occupation: Works at Dion's. (Typical of a product sponge worthless teenager.)</li>
+			<li>Technology: Owns a Samsung Galaxy s5 (OS is updated to 5.0 Lollipop and has Verison as a mobile carrier)</li>
+			<li>Needs: As a teenager currently in High School and working at a glorified cooked dough and tomato shop his main needs are to escape his reality and substitute his own and to take out his pent up rage and hormone imbalances and take them out on random strangers on the internet who are only looking for a little bit of entertainment to distract themselves from their dead end jobs as well. TL;DR: he needs a source of entertainment where he can watch videos of cats playing and insult strangers simultaneously.</li>
+			<li>Goals: His goals are to ruin the lives of everyone around him and even miles away with mean comments while watching a .gif of a kitten playing with a sock.</li>
+		</ul>
+		<p id="Use-Case">Jimmy needs to get on Reddit and post a comment on a post that someone has made. So typically he would be inside of the Reddit app on his phone watching the video of someone playing with their new kitten with one of their socks. In order to leave a comment he would need to have a username and password which would typically already be verified. Depending on where he is in the post if he is currently looking at the .gif he needs to go back one step to the core post. From there the post should normally be highlighted so he can already see the list of options for said post so all he needs to do is hit "comments." From here he will see a list of all of the comments previously made by other users and at the top there is a button that looks like a generic "text bubble." If he clicks on that the app will then send him to a new section where he can begin typing his tirade of spelling errors and hate mail to a person he doesn't even know thousands of miles away. After he is finished dumping his excess of testosterone and "douchestrogen" all he has to do is push a little button that looks like a paper airplane. (A thumbnail of the Hindenburg going down might be more apt in this scenario.) After he hits that the comment is sent, the owner is informed that they have a new comment "oh joy!" and the page will refresh showing him his masterpiece right on top of all the other well thought out and non hate crime filled comments.
+		</p>
+			<ul id="Interaction-Flow">
+			<!--This is assuming that he was already in the app, logged in, and viewing the content.-->
+			<li>1)Jimmy has to hit the back button to go back from the .gif and to the thumbnail of the post and a list of options underneath it including "comments"</li>
+			<li>2)Jimmy hits the comments button and the app loads in the comments that have already been made </li>
+		</ul>
+	</body>
+</html>
