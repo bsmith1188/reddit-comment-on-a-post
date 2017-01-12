b0aefdd5065f3e611db69a9de598896f2246c0a5
Ben Smith <rios7467@gmail.com>
2017-01-11 19:21:42 -0700

    Added a goddamn 4 that I missed and also somehow fixed the pathing by doing nothing....... WAT

--- .index.php
+++ .index.php
@@ -32,7 +32,7 @@
 			<li>2)Posts: Posts are links to images or videos hosted by another site in this case typically imgur and include the posters username and a timestamp of when it was posted.</li>
 			<li>Post-Title: Posts also must contain a title for identification purposes and human interaction.</li>
 			<li>3)Comments: Comments are cascaded down from posts and notifications are sent to the owner of the comment above, if there is no comment above it is added on top and the notification is sent to the owner of the post. Addendum: multiple comments can be added to the same comment or post. </li>
-			<li>Upvotes/Downvotes: Any post or comment can be upvoted or downvoted once per user. </li>
+			<li>4) Upvotes/Downvotes: Any post or comment can be upvoted or downvoted once per user. </li>
 		</ul>
 	</body>
 </html>
