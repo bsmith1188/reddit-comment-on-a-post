cdd2f7c5e41fdb20cc681ccc3e6e196f7e7d643c
Ben Smith <rios7467@gmail.com>
2017-01-17 16:59:41 -0700

    Added third table of comments to sql file

--- sql/reddit.sql
+++ sql/reddit.sql
@@ -1,15 +1,29 @@
 CREATE TABLE profile (
-profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-profileDateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-profileEmail VARCHAR (128) NOT NULL,
-profileUserDob DATE,
-UNIQUE (profileEmail),
-UNIQUE (profileId)
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileDateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	profileEmail VARCHAR (128) NOT NULL,
+	profileUserDob DATE,
+	UNIQUE (profileEmail),
+	UNIQUE (profileId),
+	PRIMARY KEY (profileId)
 );
 CREATE TABLE post (
-postId INT UNSIGNED NOT NULL,
-postProfileId INT UNSIGNED NOT NULL,
-postContent VARCHAR(255),
-postDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+	postId INT UNSIGNED NOT NULL,
+	postProfileId INT UNSIGNED NOT NULL,
+	postContent VARCHAR(255),
+	postDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	UNIQUE (postId),
+	UNIQUE (postProfileId),
+	PRIMARY KEY (postId),
+	FOREIGN KEY (postProfileId)
 );
-CREATE TABLE
+CREATE TABLE comment (
+	commentPostId INT UNSIGNED NOT NULL,
+	commentProfileId INT UNSIGNED NOT NULL,
+	commentContent VARCHAR(255),
+	commentDateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	UNIQUE (commentPostId),
+	UNIQUE (commentProfileId),
+	FOREIGN KEY (commentPostId),
+	FOREIGN KEY (commentProfileId)
+);
