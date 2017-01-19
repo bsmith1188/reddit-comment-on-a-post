979a43c4568cc912c95de69a3e67c697e0549837
Ben Smith <rios7467@gmail.com>
2017-01-19 09:40:56 -0700

    Added the profile class and the private quallifiers

---
+++ PHP/profile-class.php
@@ -0,0 +1,26 @@
+<?php
+namespace edu\cnm\bsmtih\bootcamp;
+/**
+* Small example of a Reddit profile and its contents.
+ *
+ * @author Benjamin Smith <bsmtih@cnm.edu>
+ *
+ */
+class Profile implements \JsonSerializable {
+	use ValidateDate;
+
+	private $profileId;
+		/** this is the users profile name, also the primary key */
+	/** @var int $profileId */
+	private $profileDateCreated;
+	/** this is for when the user makes a profile, a timestamp will mark the date of the creation of the profile */
+	/** @var int */
+	private $profileEmail;
+	/** this is the email used to make the profile and cannot be used for more than one profile */
+	/** @var int(128) */
+	private $userDob;
+	/**this is optional for the profile and is only asked for to verify use of the "nsfw" subreddits*/
+	/** @var  */
+
+
+}
